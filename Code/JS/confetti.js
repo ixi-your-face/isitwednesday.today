e334390967bd85f1f4fac757dee072b9a61da45e
stlenx <bernalencisa@gmail.com>
2023-07-11 23:35:18 +0200

    frog halfway done

--- Code/JS/confetti.js
+++ Code/JS/confetti.js
@@ -1,302 +1,190 @@
-// utilities
-function getLength(x0, y0, x1, y1) {
-    // returns the length of a line segment
-    const x = x1 - x0;
-    const y = y1 - y0;
-    return Math.sqrt(x * x + y * y);
-}
-
 function getDegAngle(x0, y0, x1, y1) {
     const y = y1 - y0;
     const x = x1 - x0;
     return Math.atan2(y, x) * (180 / Math.PI);
 }
 
-// some constants
-const DECAY = 4;        // confetti decay in seconds
-const SPREAD = 60;      // degrees to spread from the angle of the cannon
+function addClass(el, className) {
+    el.classList.add(className);
+}
+function removeClass(el, className) {
+    el.classList.remove(className);
+}
+
+const DECAY = 4;
+const SPREAD = 50;
 const GRAVITY = 1200;
 
-class ConfettiCannon {
-    constructor() {
-        // setup a canvas
-        this.canvas = document.getElementById('canvas');
-        this.dpr = window.devicePixelRatio || 1;
-        this.ctx = this.canvas.getContext('2d');
-        this.ctx.scale(this.dpr, this.dpr);
+let angle = 270;
+let shoot = false;
 
-        // add confetti here
-        this.confettiSpriteIds = [];
-        this.confettiSprites = {};
+let dpr = window.devicePixelRatio || 1;
+const tada = document.getElementById('tada');
+const shaker = document.getElementById('shaker');
+const intro = document.getElementById('intro');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-        // vector line representing the firing angle
-        this.drawVector = false;
-        this.vector = [{
-            x: window.innerWidth,
-            y: window.innerHeight * 1.25,
-        }, {
-            x: window.innerWidth,
-            y: window.innerHeight * 2,
-        }];
+let confettiSpriteIds = [];
+let confettiSprites = {};
 
-        this.pointer = {};
+ctx.scale(dpr, dpr);
 
-        // bind methods
-        this.render = this.render.bind(this);
-        this.handleMousedown = this.handleMousedown.bind(this);
-        this.handleMouseup = this.handleMouseup.bind(this);
-        this.handleMousemove = this.handleMousemove.bind(this);
-        this.handleTouchstart = this.handleTouchstart.bind(this);
-        this.handleTouchmove = this.handleTouchmove.bind(this);
-        this.setCanvasSize = this.setCanvasSize.bind(this);
+function setCanvasSize() {
+    canvas.width = window.innerWidth * dpr;
+    canvas.height = window.innerHeight * dpr;
+    // canvas.style.width = window.innerWidth + 'px';
+    // canvas.style.height = window.innerHeight + 'px';
+}
 
-        this.setupListeners();
-        this.setCanvasSize();
+function setupListeners() {
+    // Use TweenLite tick event for the render loop
+    TweenLite.ticker.addEventListener('tick', render);
 
-        // fire off for a demo
-        this.timer = setTimeout(this.handleMouseup, 1000);
-    }
+    canvas.addEventListener('mousedown', handleMousedown);
+    canvas.addEventListener('mouseup', handleMouseup);
+    canvas.addEventListener('mousemove', handleMousemove);
+    canvas.addEventListener('touchstart', handleMousedown);
+    canvas.addEventListener('touchend', handleMouseup);
+    canvas.addEventListener('touchmove', handleTouchmove);
+    canvas.addEventListener('resize', setCanvasSize);
+}
 
-    setupListeners() {
-        // Use TweenLite tick event for the render loop
-        TweenLite.ticker.addEventListener('tick', this.render);
+function handleMousemove(e) {
+    const pointerAngle = getDegAngle(canvas.width/2, canvas.height*.9, e.clientX*dpr, e.clientY*dpr);
+    angle = pointerAngle;
+    tada.style.transform = `translateX(0)rotate(${angle + 45}deg)`;
+}
+function handleTouchmove(e) {
+    const pointerAngle = getDegAngle(canvas.width/2, canvas.height*.9, e.touches[0].clientX*dpr, e.touches[0].clientY*dpr);
+    angle = pointerAngle;
+    tada.style.transform = `translateX(0)rotate(${angle + 45}deg)`;
+}
 
-        // bind events
-        window.addEventListener('mousedown', this.handleMousedown);
-        window.addEventListener('mouseup', this.handleMouseup);
-        window.addEventListener('mousemove', this.handleMousemove);
-        window.addEventListener('touchstart', this.handleTouchstart);
-        window.addEventListener('touchend', this.handleMouseup);
-        window.addEventListener('touchmove', this.handleTouchmove);
-        window.addEventListener('resize', this.setCanvasSize);
-    }
+function handleMousedown() {
+    shoot = true;
+    addClass(shaker, 'shake');
+    addClass(intro, 'bye');
+}
+function handleMouseup() {
+    shoot = false;
+    removeClass(shaker, 'shake');
+}
 
-    setCanvasSize() {
-        this.canvas.width = window.innerWidth * this.dpr;
-        this.canvas.height = window.innerHeight * this.dpr;
-        this.canvas.style.width = window.innerWidth + 'px';
-        this.canvas.style.height = window.innerHeight + 'px';
-    }
+function addConfettiParticles(amount, angle, velocity, x, y) {
+    let i = 0;
+    while (i < amount) {
+        // sprite
+        const r = _.random(4, 6) * dpr;
+        const d = _.random(15, 25) * dpr;
 
-    handleMousedown(event) {
-        clearTimeout(this.timer);
-        const x = event.clientX * this.dpr;
-        const y = event.clientY * this.dpr;
+        const cr = _.random(50, 255);
+        const cg = _.random(50, 200);
+        const cb = _.random(50, 200);
+        const color = `rgb(${cr}, ${cg}, ${cb})`;
 
-        this.vector[0] = {
-            x,
-            y,
-        };
-        this.drawVector = true;
-    }
+        const tilt = _.random(10, -10);
+        const tiltAngleIncremental = _.random(0.07, 0.05);
+        const tiltAngle = 0;
 
-    handleTouchstart(event) {
-        clearTimeout(this.timer);
-        event.preventDefault();
-        const x = event.touches[0].clientX * this.dpr;
-        const y = event.touches[0].clientY * this.dpr;
-        this.vector[0] = {
-            x,
-            y,
-        };
-
-        this.drawVector = true;
-    }
-
-    handleMouseup(event) {
-        this.drawVector = false;
-
-        const x0 = this.vector[0].x;
-        const y0 = this.vector[0].y;
-        const x1 = this.vector[1].x;
-        const y1 = this.vector[1].y;
-
-        const length = getLength(x0, y0, x1, y1);
-        const angle = getDegAngle(x0, y0, x1, y1) + 180;
-
-        const particles = length / 5 + 5;
-        const velocity = length * 10;
-        this.addConfettiParticles(particles, angle, velocity, x0, y0);
-    }
-
-    handleMousemove(event) {
-        const x = event.clientX * this.dpr;
-        const y = event.clientY * this.dpr;
-        this.vector[1] = {
-            x,
-            y,
-        };
-        this.pointer = this.vector[1];
-    }
-
-    handleTouchmove(event) {
-        event.preventDefault();
-        const x = event.changedTouches[0].clientX * this.dpr;
-        const y = event.changedTouches[0].clientY * this.dpr;
-        this.vector[1] = {
-            x,
-            y,
-        };
-        this.pointer = this.vector[1];
-    }
-
-    drawVectorLine() {
-        this.ctx.strokeStyle = 'pink';
-        this.ctx.lineWidth = 2 * this.dpr;
-
-        const x0 = this.vector[0].x;
-        const y0 = this.vector[0].y;
-        const x1 = this.vector[1].x;
-        const y1 = this.vector[1].y;
-
-        this.ctx.beginPath();
-        this.ctx.moveTo(x0, y0);
-        this.ctx.lineTo(x1, y1);
-        this.ctx.stroke();
-    }
-
-    addConfettiParticles(amount, angle, velocity, x, y) {
-        let i = 0;
-        while (i < amount) {
-            // sprite
-            const r = _.random(4, 6) * this.dpr;
-            const d = _.random(15, 25) * this.dpr;
-
-            const cr = _.random(30, 255);
-            const cg = _.random(30, 230);
-            const cb = _.random(30, 230);
-            const color = `rgb(${cr}, ${cg}, ${cb})`;
-
-            const tilt = _.random(10, -10);
-            const tiltAngleIncremental = _.random(0.07, 0.05);
-            const tiltAngle = 0;
-
-            const id = _.uniqueId();
-            const sprite = {
-                [id]: {
-                    angle,
-                    velocity,
-                    x,
-                    y,
-                    r,
-                    d,
-                    color,
-                    tilt,
-                    tiltAngleIncremental,
-                    tiltAngle,
-                },
-            };
-
-            Object.assign(this.confettiSprites, sprite);
-            this.confettiSpriteIds.push(id);
-            this.tweenConfettiParticle(id);
-            i++;
-        }
-    }
-
-    tweenConfettiParticle(id) {
-        const minAngle = this.confettiSprites[id].angle - SPREAD / 2;
-        const maxAngle = this.confettiSprites[id].angle + SPREAD / 2;
-
-        const minVelocity = this.confettiSprites[id].velocity / 4;
-        const maxVelocity = this.confettiSprites[id].velocity;
-
-        // Physics Props
-        const velocity = _.random(minVelocity, maxVelocity);
-        const angle = _.random(minAngle, maxAngle);
-        const gravity = GRAVITY;
-        const friction = _.random(0.1, 0.25);
-        const d = 0;
-
-        TweenLite.to(this.confettiSprites[id], DECAY, {
-            physics2D: {
-                velocity,
+        const id = _.uniqueId();
+        const sprite = {
+            [id]: {
                 angle,
-                gravity,
-                friction,
+                velocity,
+                x,
+                y,
+                r,
+                d,
+                color,
+                tilt,
+                tiltAngleIncremental,
+                tiltAngle,
             },
-            d,
-            ease: Power4.easeIn,
-            onComplete: () => {
-                // remove confetti sprite and id
-                _.pull(this.confettiSpriteIds, id);
-                delete this.confettiSprites[id];
-            },
-        });
-    }
+        };
 
-    updateConfettiParticle(id) {
-        const sprite = this.confettiSprites[id];
-
-        const tiltAngle = 0.0005 * sprite.d;
-
-        sprite.angle += 0.01;
-        sprite.tiltAngle += tiltAngle;
-        sprite.tiltAngle += sprite.tiltAngleIncremental;
-        sprite.tilt = (Math.sin(sprite.tiltAngle - (sprite.r / 2))) * sprite.r * 2;
-        sprite.y += Math.sin(sprite.angle + sprite.r / 2) * 2;
-        sprite.x += Math.cos(sprite.angle) / 2;
-    }
-
-    drawConfetti() {
-        this.confettiSpriteIds.map(id => {
-            const sprite = this.confettiSprites[id];
-
-            this.ctx.beginPath();
-            this.ctx.lineWidth = sprite.d / 2;
-            this.ctx.strokeStyle = sprite.color;
-            this.ctx.moveTo(sprite.x + sprite.tilt + sprite.r, sprite.y);
-            this.ctx.lineTo(sprite.x + sprite.tilt, sprite.y + sprite.tilt + sprite.r);
-            this.ctx.stroke();
-
-            this.updateConfettiParticle(id);
-        });
-    }
-
-    drawPointer() {
-        const centerX = this.pointer.x;
-        const centerY = this.pointer.y;
-        const radius = 15 * this.dpr;
-
-        this.ctx.beginPath();
-        this.ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-        this.ctx.fillStyle = 'transparent';
-        this.ctx.fill();
-        this.ctx.lineWidth = 2 * this.dpr;
-        this.ctx.strokeStyle = '#ffffff';
-        this.ctx.stroke();
-    }
-
-    drawPower() {
-        const x0 = this.vector[0].x;
-        const y0 = this.vector[0].y;
-        const x1 = this.vector[1].x;
-        const y1 = this.vector[1].y;
-
-        const length = getLength(x0, y0, x1, y1);
-        const centerX = x0;
-        const centerY = y0;
-        const radius = 1 * this.dpr * length / 20;
-
-        this.ctx.beginPath();
-        this.ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-        this.ctx.fillStyle = 'transparent';
-        this.ctx.fill();
-        this.ctx.lineWidth = 2 * this.dpr;
-        this.ctx.strokeStyle = '#333333';
-        this.ctx.stroke();
-    }
-
-    render() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-        // only draw the vector when the drawVector flag is on
-        this.drawVector && this.drawVectorLine();
-        this.drawVector && this.drawPower();
-
-        this.drawPointer();
-        this.drawConfetti();
+        Object.assign(confettiSprites, sprite);
+        confettiSpriteIds.push(id);
+        tweenConfettiParticle(id);
+        i++;
     }
 }
 
-const confetti = new ConfettiCannon();
+function tweenConfettiParticle(id) {
+    const minAngle = confettiSprites[id].angle - SPREAD / 2;
+    const maxAngle = confettiSprites[id].angle + SPREAD / 2;
+
+    const minVelocity = confettiSprites[id].velocity / 4;
+    const maxVelocity = confettiSprites[id].velocity;
+
+    // Physics Props
+    const velocity = _.random(minVelocity, maxVelocity);
+    const angle = _.random(minAngle, maxAngle);
+    const gravity = GRAVITY;
+    // const friction = _.random(0.1, 0.25);
+    const friction = _.random(0.01, 0.05);
+    const d = 0;
+
+    TweenLite.to(confettiSprites[id], DECAY, {
+        physics2D: {
+            velocity,
+            angle,
+            gravity,
+            friction,
+        },
+        d,
+        ease: Power4.easeIn,
+        onComplete: () => {
+            // remove confetti sprite and id
+            _.pull(confettiSpriteIds, id);
+            delete confettiSprites[id];
+        },
+    });
+}
+
+function updateConfettiParticle(id) {
+    const sprite = confettiSprites[id];
+
+    const tiltAngle = 0.0005 * sprite.d;
+
+    sprite.angle += 0.01;
+    sprite.tiltAngle += tiltAngle;
+    sprite.tiltAngle += sprite.tiltAngleIncremental;
+    sprite.tilt = (Math.sin(sprite.tiltAngle - (sprite.r / 2))) * sprite.r * 2;
+    sprite.y += Math.sin(sprite.angle + sprite.r / 2) * 2;
+    sprite.x += Math.cos(sprite.angle) / 2;
+}
+
+function drawConfetti() {
+    confettiSpriteIds.map(id => {
+        const sprite = confettiSprites[id];
+
+        ctx.beginPath();
+        ctx.lineWidth = sprite.d / 2;
+        ctx.strokeStyle = sprite.color;
+        ctx.moveTo(sprite.x + sprite.tilt + sprite.r, sprite.y);
+        ctx.lineTo(sprite.x + sprite.tilt, sprite.y + sprite.tilt + sprite.r);
+        ctx.stroke();
+
+        updateConfettiParticle(id);
+    });
+}
+
+function render() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawConfetti();
+}
+
+function shootConfetti() {
+    requestAnimationFrame(shootConfetti);
+    if (shoot) {
+        addConfettiParticles(10, angle, 5000, canvas.width/2, canvas.height*.9);
+    }
+}
+
+setupListeners();
+setCanvasSize();
+// let foo = addConfettiParticles(100, 0, 5000, window.innerWidth/2, window.innerHeight/2);
+
+shootConfetti();
---
+++ Code/JS/frog.js
@@ -0,0 +1,77 @@
+let frogContainer = document.getElementById("frog-container");
+let frogs = [];
+let baseSize = 69;
+
+class Frog {
+    constructor(x, y, velX, velY, rSpeed, size) {
+        this.element = document.createElement("img");
+        this.element.src = "Media/Images/frog.webp";
+        this.element.style.width = `${baseSize * size}px`;
+        frogContainer.appendChild(this.element);
+
+        this.x = x;
+        this.y = y;
+
+        this.velocityX = velX;
+        this.velocityY = velY;
+
+        this.rotation = 0;
+        this.rotationSpeed = rSpeed;
+        this.size = baseSize * size;
+    }
+
+    Update() {
+        this.x += this.velocityX;
+        this.y += this.velocityY;
+        this.rotation += this.rotationSpeed;
+
+        if(this.x < -this.size) {
+            this.x = window.innerWidth + this.size;
+        }
+        if(this.x > window.innerWidth + this.size) {
+            this.x = -this.size;
+        }
+
+        if(this.y < -this.size) {
+            this.y = window.innerHeight + this.size;
+        }
+        if(this.y > window.innerHeight + this.size) {
+            this.y = -this.size;
+        }
+
+        this.element.style.left = `${this.x}px`;
+        this.element.style.top = `${this.y}px`;
+        this.element.style.transform = ``;
+    }
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function Update() {
+    frogs.forEach((frog => {
+        frog.Update();
+    }));
+
+
+    window.requestAnimationFrame(Update);
+}
+
+window.requestAnimationFrame(Update);
+
+function AddFrog() {
+    frogs.push(new Frog(
+        getRandomArbitrary(0, window.innerWidth),
+        getRandomArbitrary(0, window.innerHeight),
+        getRandomArbitrary(-1.2, 1.2),
+        getRandomArbitrary(-1.2, 1.2),
+        getRandomArbitrary(-1, 1),
+        getRandomArbitrary(1, 1.2)
+    ));
+}
+
+for(let i=0; i < 6; i++) {
+    AddFrog();
+}
+
