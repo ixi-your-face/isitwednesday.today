20f0d93b240e8bea4b2143161808fc9bc4191257
stlenx <bernalencisa@gmail.com>
2023-07-12 00:18:03 +0200

    It is wednesday my dudes
    AAAAAAAAAAAAAAAAAA

--- Code/JS/main.js
+++ Code/JS/main.js
@@ -1,9 +1,14 @@
 let main = document.getElementsByClassName("main-container")[0];
-
+let clicker = document.getElementById("landing");
 
 function removeClasses() {
     main.classList.remove("flip-yes");
     main.classList.remove("flip-no");
+}
+
+function PlayAudio(path) {
+    let audio = new Audio(path);
+    audio.play();
 }
 
 function flipYES() {
@@ -15,6 +20,27 @@ function flipYES() {
         for(let i=0; i < 20; i++) {
             AddFrog();
         }
+
+        let duration = 500;
+        if(Math.random() > 0.5) {
+            PlayAudio('Media/Audio/airhorn.mp3');
+
+            duration = 3000;
+        } else {
+            PlayAudio('Media/Audio/partyblower.mp3');
+
+            duration = 1000;
+        }
+
+
+        setTimeout(() => {
+            if(Math.random() < 0.98) {
+                PlayAudio('Media/Audio/iwmd-full.mp3');
+            } else {
+                PlayAudio('Media/Audio/iwmd-dance.mp3');
+            }
+        }, duration)
+
     }, 4000)
 }
 
@@ -26,8 +52,16 @@ function flipNO() {
 const date = new Date();
 let day = date.getDay();
 
-if(day === 3) {
-    flipYES();
-} else {
-    flipNO();
-}
+
+clicker.addEventListener("click", () => {
+    setTimeout(() => {
+        clicker.style.opacity = "0";
+        document.getElementById("no").style.opacity = "100%";
+    }, 500)
+
+    if(day === 3) {
+        flipYES();
+    } else {
+        flipNO();
+    }
+})
